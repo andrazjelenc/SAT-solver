5d7ea7abddb19a543cccddb4cc229bf8231c8844
marancinas <50864997+marancinas@users.noreply.github.com>
2020-03-17 17:59:38 +0100

    added some tests
    new sudoku examples and n_queens

--- test.py
+++ test.py
@@ -12,6 +12,7 @@ def check_solutions(sol1, sol2):
     for key in keys1.intersection(keys2):
         len_intersect += 1
         if sol1[key] != sol2[key]:
+            print('Error: key {} has the wrong value. Our solution: {} - legit: {}'.format(key, sol1[key], sol2[key]))
             return False
 
     print("Size of our solution is {}, size of legit solution is {}. All okey in intersection, where is {} keys".format(len1, len2, len_intersect))
@@ -46,6 +47,39 @@ clauses_list = p.parse()
 s = DPLLSolver(clauses_list)
 status, solution, _ = s.solve()
 p = Parser('examples/sudoku_mini_solution.txt')
+legit_solution = p.parse_solution()
+
+result = check_solutions(solution, legit_solution)
+print(result)
+
+#sudoku hard
+p = Parser('examples/sudoku_124.txt')
+clauses_list = p.parse()
+s = DPLLSolver(clauses_list)
+status, solution, _ = s.solve()
+p = Parser('examples/sudoku_124_solution.txt')
+legit_solution = p.parse_solution()
+
+result = check_solutions(solution, legit_solution)
+print(result)
+
+#sudoku extra hard
+p = Parser('examples/sudoku_extra.txt')
+clauses_list = p.parse()
+s = DPLLSolver(clauses_list)
+status, solution, _ = s.solve()
+p = Parser('examples/sudoku_extra_solution.txt')
+legit_solution = p.parse_solution()
+
+result = check_solutions(solution, legit_solution)
+print(result)
+
+# 8_queens
+p = Parser('examples/8_queens.txt')
+clauses_list = p.parse()
+s = DPLLSolver(clauses_list)
+status, solution, _ = s.solve()
+p = Parser('examples/8_queens_solution.txt')
 legit_solution = p.parse_solution()
 
 result = check_solutions(solution, legit_solution)
